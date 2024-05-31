e48f309bcba981bf3922c0089706cf7bdde839c1
Anhar Risnumawan <anhrisn@gmail.com>
2024-05-31 11:40:37 +0900

    Update plot of training and validation loss

--- test/plotTrainingLoss.m
+++ test/plotTrainingLoss.m
@@ -1,3 +1,5 @@
+% Existing code
+close all
 % Load trainInfo.mat
 
 % Plot the training and validation loss
@@ -8,13 +10,19 @@ plot(trainInfo.ValidationHistory.Loss, 'r--', 'LineWidth', 2);
 hold off;
 
 % Set x-axis range
-xlim([800, 1500]);
+xlim([20, 150]);
 % ylim([0, 1]);
 
+grid on;
+
 % Add labels and title
-xlabel('Iteration');
-ylabel('Loss');
-title('Training and Validation Loss');
+xlabel('Iteration', 'FontSize', 14);
+ylabel('Loss', 'FontSize', 14);
 
 % Add legend
-legend('Training Loss', 'Validation Loss');
+legend('Training Loss', 'Validation Loss', 'FontSize', 14);
+
+% New code to increase font size of x and y axis tick values
+ax = gca; % get current axes
+ax.XAxis.FontSize = 12; % set x-axis font size
+ax.YAxis.FontSize = 12; % set y-axis font size
