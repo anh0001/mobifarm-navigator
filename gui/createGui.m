1b9409216cafddbcd4f7983fa55d70d30a3a5ded
Anhar Risnumawan <anhrisn@gmail.com>
2024-06-03 21:16:58 +0900

    Add ground truth labels for the GUI

--- gui/createGui.m
+++ gui/createGui.m
@@ -12,16 +12,23 @@ function createGui(model, dataVal)
     uilabel(fig, 'Position', [400 400 100 30], 'Text', 'Angular Velocity:');
     angVelLabel = uilabel(fig, 'Position', [500 400 100 30], 'Text', '');
 
+    % Create labels for displaying ground truth
+    uilabel(fig, 'Position', [400 350 150 30], 'Text', 'Ground Truth Linear Velocity:');
+    linVelGTLabel = uilabel(fig, 'Position', [550 350 100 30], 'Text', '');
+
+    uilabel(fig, 'Position', [400 300 150 30], 'Text', 'Ground Truth Angular Velocity:');
+    angVelGTLabel = uilabel(fig, 'Position', [550 300 100 30], 'Text', '');
+
     % Button for starting image display and prediction
     btnStart = uibutton(fig, 'Position', [200 50 100 30], 'Text', 'Start', ...
-        'ButtonPushedFcn', @(btnStart,event) startImageDisplay(ax, linVelLabel, angVelLabel, model, dataVal));
+        'ButtonPushedFcn', @(btnStart,event) startImageDisplay(ax, linVelLabel, angVelLabel, linVelGTLabel, angVelGTLabel, model, dataVal));
 
     % Button for stopping image display
     btnStop = uibutton(fig, 'Position', [350 50 100 30], 'Text', 'Stop', ...
         'ButtonPushedFcn', @(btnStop,event) stopImageDisplay());
 end
 
-function startImageDisplay(ax, linVelLabel, angVelLabel, model, dataVal)
+function startImageDisplay(ax, linVelLabel, angVelLabel, linVelGTLabel, angVelGTLabel, model, dataVal)
     % Ensure the tools directory is on the path
     addpath(genpath('src'));   % Add source code directory
     addpath(genpath('lib'));   % Add library directory
@@ -38,6 +45,7 @@ function startImageDisplay(ax, linVelLabel, angVelLabel, model, dataVal)
             img = data{1};    % RGB image
             lidar = data{2};  % Distance map (Lidar)
             pcd = data{3};    % Point cloud data
+            groundTruth = data{4}; % Ground truth labels
             
             imshow(img, 'Parent', ax);
 
@@ -58,9 +66,17 @@ function startImageDisplay(ax, linVelLabel, angVelLabel, model, dataVal)
             linearVelocity = prediction(1);
             angularVelocity = prediction(2);
 
+            % Extract ground truth velocities
+            groundTruthLinVel = groundTruth(1);
+            groundTruthAngVel = groundTruth(2);
+
             % Update labels with predictions
             linVelLabel.Text = num2str(linearVelocity);
             angVelLabel.Text = num2str(angularVelocity);
+
+            % Update labels with ground truth
+            linVelGTLabel.Text = num2str(groundTruthLinVel);
+            angVelGTLabel.Text = num2str(groundTruthAngVel);
 
             pause(0.5);  % Adjust the pause duration as needed
         end
